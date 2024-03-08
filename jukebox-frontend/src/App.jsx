314e777847176b9deec6d6858dc94cb611f455de
Ahmet <96299039+ahmtguler@users.noreply.github.com>
2024-03-08 19:47:42 +0300

    frontend first layout draft
    some divs and boxes trying to find a nice layout

---
+++ jukebox-frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                <div className="Chat">
+                    
+                </div>
+                <div className="Player">
+                        <div className="CurrentlyPlaying"></div>
+                        <div className="Queue"></div>
+                    </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
